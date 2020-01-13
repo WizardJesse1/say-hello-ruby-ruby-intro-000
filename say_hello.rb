ac419080b87ed4332745517c310f5e906819525d
Jesse Woolsey <jesse232@live.com>
2020-01-13 01:42:18 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,9 @@
 # Build your say_hello method here
 
-def say_hello(name="Hello Ruby Programmer!"")
+def say_hello(name="Hello Ruby Programmer!")
   puts "Hello #{name}!"
 end
 
 say_hello("Gabriela")
+
+say_hello
